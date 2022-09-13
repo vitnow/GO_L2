2ba8eb7f92cd71b6b109c069782c8d39572aeb38
ViToN <vitnow2009@gmail.com>
2022-09-13 23:24:52 +0500

    add config_windows

--- config_windows.go
+++ config_windows.go
@@ -1,1 +1,16 @@
+//go:build !windows
+// +build !windows
+
 package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
+func getConfig() Config {
+	data, _ := ioutil.ReadFile("C:\\Users\\vitno\\Documents\\Config\\config.json")
+	var c Config
+	_ = json.Unmarshal(data, &c)
+	return c
+}
