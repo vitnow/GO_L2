8fce93895c3d1ab7f8e51f52b8ec8bf210b6f068
ViToN <vitnow2009@gmail.com>
2022-09-21 11:52:27 +0500

    add vendor

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/vitnow/GO_L2/tree/Lesson_3
 
 go 1.18
+
+require (
+	github.com/gorilla/websocket v1.5.0
+	github.com/valyala/fasthttp v1.40.0
+)
--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package GO_L2
 
-import "github.com/vitnow/GO_L2/tree/Lesson_3"
+//import Lesson_3 "github.com/vitnow/GO_L2/tree/Lesson_3"
+import (
+	_ "github.com/valyala/fasthttp"
+	_ "github.com/gorilla/websocket"
+)
 
 func main() {
 
